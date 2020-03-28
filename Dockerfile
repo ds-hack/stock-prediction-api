1db5790fe186ba8e94fc15726e0d7c39266c8b11
りーぐる <datascientist.toolbox.com@gmail.com>
2020-03-29 00:16:22 +0900

    chore: add docs directory

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,9 @@ RUN apk add --update --no-cache ca-certificates git gcc
 
 RUN mkdir /app
 WORKDIR /app
-ADD src/gen .
+ADD . .
 
-# RUN go mod download
-RUN go get -u github.com/gin-gonic/gin
+RUN go mod download
 RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -installsuffix cgo -o /go/bin/app
 
 FROM scratch
--- main.go
+++ main.go
@@ -3,14 +3,30 @@ package main
 import (
 	// "log"
 	// "github.com/gin-gonic/gin"
-	// "github.com/swaggo/files"
-	// "github.com/swaggo/gin-swagger"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 
-	// "./docs"
+	_ "github.com/ds-hack/stock-prediction-api/docs"
 	"github.com/ds-hack/stock-prediction-api/routers"
 )
 
+// @title Stock Prediction API
+// @version 1.0
+// @description Demonstration api for dshack project
+
+// @contact.name API Support
+// @contact.url https://datascientist-toolbox.com/
+// @contact.email datascientist.toolbox.com@gmail.com
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host localhost:8080
+// @BasePath /api/v1
+// @query.collection.format multi
 func main() {
 	router := routers.InitRouter()
+
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	router.Run(":8080")
 }
